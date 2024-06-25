c0088665db8609416356454aa1d5f6600d906b1d
Thomas Hegghammer <64712218+Hegghammer@users.noreply.github.com>
2024-06-25 06:46:41 +0100

    add typst

--- .devcontainer/update-content.sh
+++ .devcontainer/update-content.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt -y upgrade
 pip install --upgrade setuptools
+curl -fsSL https://typst.community/typst-install/install.sh | sh
+echo 'export TYPST_INSTALL="/home/vscode/.typst"' >> ~/.bashrc
+echo 'export PATH="$TYPST_INSTALL/bin:$PATH"' >> ~/.bashrc

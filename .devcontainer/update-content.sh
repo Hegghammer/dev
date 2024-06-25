0106b459fcf42c7ea4c5a2b22ce5966b3e8edade
Thomas Hegghammer <64712218+Hegghammer@users.noreply.github.com>
2024-06-25 22:49:22 +0100

    add ssh-keygen

--- .devcontainer/update-content.sh
+++ .devcontainer/update-content.sh
@@ -21,3 +21,6 @@ echo 'export PATH="/home/vscode/.pandoc:$PATH"' >> ~/.bashrc
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && sudo apt update \
 && sudo apt install gh -y
+
+# Create ssh key
+yes '' | ssh-keygen -N '' > /dev/null

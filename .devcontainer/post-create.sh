45c8734e432b4ae67d100f35111b53cd48163087
Hegghammer <64712218+Hegghammer@users.noreply.github.com>
2025-05-07 12:06:28 +0100

    add separate settings.json

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 ln -sf $PWD/.devcontainer/.bash_aliases $HOME/.bash_aliases
 ln -sf $PWD/.devcontainer/.Rprofile $HOME/.Rprofile
-rm $HOME/.vscode-remote/data/Machine/settings.json
-ln -sf $PWD/.devcontainer/settings.json $HOME/.vscode-remote/data/Machine/settings.json

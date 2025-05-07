17eb5a28c158d3af465a5ccaebebaac5e447f98e
Thomas Hegghammer <64712218+Hegghammer@users.noreply.github.com>
2025-05-07 10:11:29 +0100

    remove github-cli

--- .devcontainer/update-content.sh
+++ .devcontainer/update-content.sh
@@ -17,13 +17,13 @@ echo 'export PATH="/home/vscode/.pandoc:$PATH"' >> ~/.bashrc
 quarto install tool tinytex
 
 # Install github-cli
-(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
-&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+# (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+# && sudo mkdir -p -m 755 /etc/apt/keyrings \
+# && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+# && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+# && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+# && sudo apt update \
+# && sudo apt install gh -y
 
 # Create ssh key
 yes '' | ssh-keygen -N '' > /dev/null
